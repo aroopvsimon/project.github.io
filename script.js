55ecd0ca7798058c09ef382c066c5d9914d1699c
aroop simon <73954384+aroopvsimon@users.noreply.github.com>
2024-01-18 22:34:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function addRow() {
     var table = document.getElementById('data-table');
     var newRow = table.insertRow(table.rows.length);
-    
+
+    // Add input fields with descriptions
+    var descriptions = ['Description 1', 'Description 2', 'Description 3'];
+
     for (var i = 0; i < 3; i++) {
         var cell = newRow.insertCell(i);
-        var input = document.createElement('input');
-        input.type = 'text';
-        cell.appendChild(input);
+        cell.innerHTML = '<input type="text" placeholder="' + descriptions[i] + '">';
     }
 
+    // Add Calculate button
     var resultCell = newRow.insertCell(3);
     resultCell.innerHTML = '<button onclick="calculateResult(this)">Calculate</button>';
 }
@@ -16,12 +18,13 @@ function addRow() {
 function calculateResult(button) {
     var row = button.parentNode.parentNode;
     var inputs = row.getElementsByTagName('input');
-    
+
     var result = 0;
     for (var i = 0; i < inputs.length; i++) {
         var inputValue = parseFloat(inputs[i].value) || 0;
         result += inputValue;
     }
 
+    // Display the result in the last cell
     row.cells[row.cells.length - 1].innerHTML = result;
 }
