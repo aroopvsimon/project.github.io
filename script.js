1281a41dccf7c12ca8918e0de60951eaa7281a88
aroop simon <73954384+aroopvsimon@users.noreply.github.com>
2024-01-18 22:27:13 +0530

    user input for three fields and displays the result back on the web page

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function addRow() {
     var table = document.getElementById('data-table');
     var newRow = table.insertRow(table.rows.length);
-    var columnsCount = table.rows[0].cells.length;
-
-    for (var i = 0; i < columnsCount; i++) {
+    
+    for (var i = 0; i < 3; i++) {
         var cell = newRow.insertCell(i);
         var input = document.createElement('input');
         input.type = 'text';
         cell.appendChild(input);
     }
+
+    var resultCell = newRow.insertCell(3);
+    resultCell.innerHTML = '<button onclick="calculateResult(this)">Calculate</button>';
+}
+
+function calculateResult(button) {
+    var row = button.parentNode.parentNode;
+    var inputs = row.getElementsByTagName('input');
+    
+    var result = 0;
+    for (var i = 0; i < inputs.length; i++) {
+        var inputValue = parseFloat(inputs[i].value) || 0;
+        result += inputValue;
+    }
+
+    row.cells[row.cells.length - 1].innerHTML = result;
 }
