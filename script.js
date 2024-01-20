9bb6c01951f2e9952b9008098fd3122e10184830
aroop simon <73954384+aroopvsimon@users.noreply.github.com>
2024-01-20 20:49:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,14 +81,16 @@ function updateSummaryTable() {
     // Update summary table cells
     summaryRow.cells[0].textContent = totalRawStorage.toFixed(2);
     summaryRow.cells[1].textContent = totalCoresForLicenses.toFixed(2);
-    summaryRow.cells[2].textContent = totalCoresForLicenses.toFixed(2); // Updated to match the sum of 'Total number of cores per Node'
-
+    
     // Calculate and set vSAN Add On Capacity Required and Difference
     var vsanAddOnCapacityRequiredCell = summaryRow.cells[2];
     var difference = totalRawStorage - totalCoresForLicenses;
 
     vsanAddOnCapacityRequiredCell.textContent = difference > 0 ? 'Yes (' + difference.toFixed(2) + ')' : 'No';
 
+    // Update VCF Storage Entitled cell
+    summaryRow.cells[3].textContent = totalCoresForLicenses.toFixed(2);
+    
     // Call calculateVsanAddOnCapacity function
     calculateVsanAddOnCapacity(totalRawStorage, parseFloat(summaryRow.cells[3].textContent));
 }
